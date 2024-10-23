705535a542a49e8b502e09582ac06d7be5dbf0ce
umerfaro <muhammadranaumerofficial@gmail.com>
2024-10-23 10:13:02 -0700

    chatBOt update

--- script.js
+++ script.js
@@ -416,80 +416,196 @@ function updateCharts(data) {
   
 
 // Event listener for df-user-input-entered
+// // Event listener for df-user-input-entered
+// window.addEventListener('df-user-input-entered', function (event) {
+//     const message = event.detail.input;
+//     const lowercaseMessage = message.toLowerCase();
+  
+//     // Check if the message is asking about temperatures
+//     if (
+//       lowercaseMessage.includes('highest temperature') ||
+//       lowercaseMessage.includes('lowest temperature') ||
+//       lowercaseMessage.includes('average temperature') || lowercaseMessage.includes('current temperature') 
+//     ) {
+//       // Prevent the message from being sent to Dialogflow
+//       event.preventDefault();
+//       event.stopImmediatePropagation();
+  
+//       if (forecastData.length > 0 || currentWeatherData) {
+//         let responseText = '';
+
+//         if (lowercaseMessage.includes('current temperature')) {
+//           if (currentWeatherData) {
+//             let temperature = currentWeatherData.main.temp;
+//             temperature = convertTemperature(temperature).toFixed(1);
+//             const cityName = currentWeatherData.name;
+//             const weatherDescription = currentWeatherData.weather[0].description;
+  
+//             responseText += `The current temperature in ${cityName} is ${temperature}°${selectedUnit}. The weather is ${weatherDescription}.\n`;
+//           } else {
+//             responseText += 'Current weather data is not available. Please fetch the weather data first.\n';
+//           }
+//         }
+  
+//         if (lowercaseMessage.includes('highest temperature')) {
+//           // Find the entry with the highest temperature
+//           const highestTempEntry = forecastData.reduce((maxEntry, currentEntry) => {
+//             return currentEntry.main.temp > maxEntry.main.temp ? currentEntry : maxEntry;
+//           }, forecastData[0]);
+  
+//           let highestTemp = highestTempEntry.main.temp;
+//           highestTemp = convertTemperature(highestTemp).toFixed(1);
+//           const [dateStr, time] = highestTempEntry.dt_txt.split(' ');
+//           const weatherDescription = highestTempEntry.weather[0].description;
+  
+//           // Get the day of the week
+//           const dateObj = new Date(highestTempEntry.dt_txt);
+//           const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//           const dayOfWeek = daysOfWeek[dateObj.getDay()];
+  
+//           responseText += `The highest temperature this week is ${highestTemp}°${selectedUnit} on ${dayOfWeek} (${dateStr}) at ${time}. The weather was ${weatherDescription}.\n`;
+//         }
+  
+//         if (lowercaseMessage.includes('lowest temperature')) {
+//           // Find the entry with the lowest temperature
+//           const lowestTempEntry = forecastData.reduce((minEntry, currentEntry) => {
+//             return currentEntry.main.temp < minEntry.main.temp ? currentEntry : minEntry;
+//           }, forecastData[0]);
+  
+//           let lowestTemp = lowestTempEntry.main.temp;
+//           lowestTemp = convertTemperature(lowestTemp).toFixed(1);
+//           const [dateStr, time] = lowestTempEntry.dt_txt.split(' ');
+//           const weatherDescription = lowestTempEntry.weather[0].description;
+  
+//           // Get the day of the week
+//           const dateObj = new Date(lowestTempEntry.dt_txt);
+//           const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//           const dayOfWeek = daysOfWeek[dateObj.getDay()];
+  
+//           responseText += `The lowest temperature this week is ${lowestTemp}°${selectedUnit} on ${dayOfWeek} (${dateStr}) at ${time}. The weather was ${weatherDescription}.\n`;
+//         }
+  
+//         if (lowercaseMessage.includes('average temperature')) {
+//           // Compute the average temperature
+//           const temperatures = forecastData.map(entry => entry.main.temp);
+//           let averageTemp = temperatures.reduce((a, b) => a + b, 0) / temperatures.length;
+//           averageTemp = convertTemperature(averageTemp).toFixed(1);
+  
+//           responseText += `The average temperature this week is ${averageTemp}°${selectedUnit}.\n`;
+//         }
+        
+  
+//         // Display the response in the chatbot
+//         addCustomResponseToChatbot(responseText);
+//       } else {
+//         addCustomResponseToChatbot(
+//           'Temperature data is not available. Please fetch the weather data first.'
+//         );
+//       }
+//     }
+//   });
+
+
 // Event listener for df-user-input-entered
 window.addEventListener('df-user-input-entered', function (event) {
-    const message = event.detail.input;
-    const lowercaseMessage = message.toLowerCase();
-  
-    // Check if the message is asking about temperatures
-    if (
-      lowercaseMessage.includes('highest temperature') ||
-      lowercaseMessage.includes('lowest temperature') ||
-      lowercaseMessage.includes('average temperature')
-    ) {
-      // Prevent the message from being sent to Dialogflow
-      event.preventDefault();
-      event.stopImmediatePropagation();
-  
-      if (forecastData.length > 0) {
-        let responseText = '';
-  
-        if (lowercaseMessage.includes('highest temperature')) {
-          // Find the entry with the highest temperature
-          const highestTempEntry = forecastData.reduce((maxEntry, currentEntry) => {
-            return currentEntry.main.temp > maxEntry.main.temp ? currentEntry : maxEntry;
-          }, forecastData[0]);
-  
-          let highestTemp = highestTempEntry.main.temp;
-          highestTemp = convertTemperature(highestTemp).toFixed(1);
-          const [dateStr, time] = highestTempEntry.dt_txt.split(' ');
-          const weatherDescription = highestTempEntry.weather[0].description;
-  
-          // Get the day of the week
-          const dateObj = new Date(highestTempEntry.dt_txt);
-          const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-          const dayOfWeek = daysOfWeek[dateObj.getDay()];
-  
-          responseText += `The highest temperature this week is ${highestTemp}°${selectedUnit} on ${dayOfWeek} (${dateStr}) at ${time}. The weather was ${weatherDescription}.\n`;
+  const message = event.detail.input;
+  const lowercaseMessage = message.toLowerCase();
+
+  // Define keywords for each query type
+  const highestTempKeywords = ['highest temperature', 'hottest', 'max temp', 'maximum temperature', 'warmest', 'peak temperature', 'high temp', 'hottest day'];
+  const lowestTempKeywords = ['lowest temperature', 'coldest', 'min temp', 'minimum temperature', 'coolest', 'low temp', 'coldest day'];
+  const averageTempKeywords = ['average temperature', 'mean temperature', 'average temp', 'typical temperature', 'usual temperature'];
+  const currentTempKeywords = ['current temperature', 'temperature now', 'temp now', 'what is the temperature', 'how hot is it', 'how cold is it', 'what\'s the temp', 'current temp'];
+
+  // Function to check if the message contains any of the keywords
+  function messageContainsKeywords(message, keywords) {
+    return keywords.some(keyword => message.includes(keyword));
+  }
+
+  // Check if the message is asking about temperatures
+  if (
+    messageContainsKeywords(lowercaseMessage, highestTempKeywords) ||
+    messageContainsKeywords(lowercaseMessage, lowestTempKeywords) ||
+    messageContainsKeywords(lowercaseMessage, averageTempKeywords) ||
+    messageContainsKeywords(lowercaseMessage, currentTempKeywords)
+  ) {
+    // Prevent the message from being sent to Dialogflow
+    event.preventDefault();
+    event.stopImmediatePropagation();
+
+    if (forecastData.length > 0 || currentWeatherData) {
+      let responseText = '';
+
+      if (messageContainsKeywords(lowercaseMessage, currentTempKeywords)) {
+        if (currentWeatherData) {
+          let temperature = currentWeatherData.main.temp;
+          temperature = convertTemperature(temperature).toFixed(1);
+          const cityName = currentWeatherData.name;
+          const weatherDescription = currentWeatherData.weather[0].description;
+
+          responseText += `The current temperature in ${cityName} is ${temperature}°${selectedUnit}. The weather is ${weatherDescription}.\n`;
+        } else {
+          responseText += 'Current weather data is not available. Please fetch the weather data first.\n';
         }
-  
-        if (lowercaseMessage.includes('lowest temperature')) {
-          // Find the entry with the lowest temperature
-          const lowestTempEntry = forecastData.reduce((minEntry, currentEntry) => {
-            return currentEntry.main.temp < minEntry.main.temp ? currentEntry : minEntry;
-          }, forecastData[0]);
-  
-          let lowestTemp = lowestTempEntry.main.temp;
-          lowestTemp = convertTemperature(lowestTemp).toFixed(1);
-          const [dateStr, time] = lowestTempEntry.dt_txt.split(' ');
-          const weatherDescription = lowestTempEntry.weather[0].description;
-  
-          // Get the day of the week
-          const dateObj = new Date(lowestTempEntry.dt_txt);
-          const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-          const dayOfWeek = daysOfWeek[dateObj.getDay()];
-  
-          responseText += `The lowest temperature this week is ${lowestTemp}°${selectedUnit} on ${dayOfWeek} (${dateStr}) at ${time}. The weather was ${weatherDescription}.\n`;
-        }
-  
-        if (lowercaseMessage.includes('average temperature')) {
-          // Compute the average temperature
-          const temperatures = forecastData.map(entry => entry.main.temp);
-          let averageTemp = temperatures.reduce((a, b) => a + b, 0) / temperatures.length;
-          averageTemp = convertTemperature(averageTemp).toFixed(1);
-  
-          responseText += `The average temperature this week is ${averageTemp}°${selectedUnit}.\n`;
-        }
-  
-        // Display the response in the chatbot
-        addCustomResponseToChatbot(responseText);
-      } else {
-        addCustomResponseToChatbot(
-          'Temperature data is not available. Please fetch the weather data first.'
-        );
       }
+
+      if (messageContainsKeywords(lowercaseMessage, highestTempKeywords)) {
+        // Find the entry with the highest temperature
+        const highestTempEntry = forecastData.reduce((maxEntry, currentEntry) => {
+          return currentEntry.main.temp > maxEntry.main.temp ? currentEntry : maxEntry;
+        }, forecastData[0]);
+
+        let highestTemp = highestTempEntry.main.temp;
+        highestTemp = convertTemperature(highestTemp).toFixed(1);
+        const [dateStr, time] = highestTempEntry.dt_txt.split(' ');
+        const weatherDescription = highestTempEntry.weather[0].description;
+
+        // Get the day of the week
+        const dateObj = new Date(highestTempEntry.dt_txt);
+        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const dayOfWeek = daysOfWeek[dateObj.getDay()];
+
+        responseText += `The highest temperature this week is ${highestTemp}°${selectedUnit} on ${dayOfWeek} (${dateStr}) at ${time}. The weather will be ${weatherDescription}.\n`;
+      }
+
+      if (messageContainsKeywords(lowercaseMessage, lowestTempKeywords)) {
+        // Find the entry with the lowest temperature
+        const lowestTempEntry = forecastData.reduce((minEntry, currentEntry) => {
+          return currentEntry.main.temp < minEntry.main.temp ? currentEntry : minEntry;
+        }, forecastData[0]);
+
+        let lowestTemp = lowestTempEntry.main.temp;
+        lowestTemp = convertTemperature(lowestTemp).toFixed(1);
+        const [dateStr, time] = lowestTempEntry.dt_txt.split(' ');
+        const weatherDescription = lowestTempEntry.weather[0].description;
+
+        // Get the day of the week
+        const dateObj = new Date(lowestTempEntry.dt_txt);
+        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const dayOfWeek = daysOfWeek[dateObj.getDay()];
+
+        responseText += `The lowest temperature this week is ${lowestTemp}°${selectedUnit} on ${dayOfWeek} (${dateStr}) at ${time}. The weather will be ${weatherDescription}.\n`;
+      }
+
+      if (messageContainsKeywords(lowercaseMessage, averageTempKeywords)) {
+        // Compute the average temperature
+        const temperatures = forecastData.map(entry => entry.main.temp);
+        let averageTemp = temperatures.reduce((a, b) => a + b, 0) / temperatures.length;
+        averageTemp = convertTemperature(averageTemp).toFixed(1);
+
+        responseText += `The average temperature this week is ${averageTemp}°${selectedUnit}.\n`;
+      }
+
+      // Display the response in the chatbot
+      addCustomResponseToChatbot(responseText);
+    } else {
+      addCustomResponseToChatbot(
+        'Temperature data is not available. Please fetch the weather data first.'
+      );
     }
-  });
+  }
+});
+
   
   
 
@@ -502,3 +618,44 @@ function addCustomResponseToChatbot(text) {
     console.error('renderCustomText method is not available on df-messenger');
   }
 }
+
+
+
+// Function to show the dashboard and hide the table
+function showDashboard() {
+  document.getElementById('dashboardSection').classList.remove('hidden');
+  document.getElementById('dashboardSection').classList.add('block');
+
+  document.getElementById('tableSection').classList.remove('block');
+  document.getElementById('tableSection').classList.add('hidden');
+
+  AOS.refresh();
+
+}
+
+// Function to show the table and hide the dashboard
+function showTable() {
+  document.getElementById('tableSection').classList.remove('hidden');
+  document.getElementById('tableSection').classList.add('block');
+
+  document.getElementById('dashboardSection').classList.remove('block');
+  document.getElementById('dashboardSection').classList.add('hidden');
+AOS.refresh();
+}
+
+// Event listeners for navbar buttons
+document.getElementById('dashboardNavBtn').addEventListener('click', showDashboard);
+document.getElementById('tablesNavBtn').addEventListener('click', showTable);
+
+// Event listeners for mobile navbar buttons
+document.getElementById('mobileDashboardNavBtn').addEventListener('click', function() {
+  showDashboard();
+  // Close mobile sidebar
+  document.getElementById('mobileSidebar').classList.add('hidden');
+});
+
+document.getElementById('mobileTablesNavBtn').addEventListener('click', function() {
+  showTable();
+  // Close mobile sidebar
+  document.getElementById('mobileSidebar').classList.add('hidden');
+});
